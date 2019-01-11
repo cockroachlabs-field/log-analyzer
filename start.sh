c437455c1d963729b7b253a20782f0c08dbd983e
Tim Veil <tjveil@gmail.com>
2019-01-10 19:50:13 -0500

    getting close.  times are correct just not days

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-docker-compose up
-
-echo "waiting for kafka and zookeeper to start..."
-sleep 20
-
-docker-compose exec splunk /opt/splunk/bin/splunk add monitor /tmp/roachlogs/.../*.log
-docker-compose exec splunk /opt/splunk/bin/splunk help datastore
+docker-compose up -d
